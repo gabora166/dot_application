756766c6bf163e3eef016e7041750ab20d496266
spidertunez <naghammahmoud199@gmail.coom>
2025-03-10 01:32:27 +0200

    sp

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: SurveyScreen(),
+        body: SplashScreen(),
       ),
     );
   }
